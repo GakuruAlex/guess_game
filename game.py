a42d69514f90869f6a295e4643fb7b44a5e50d7f
Alex Gakuru <gakurualexgicimo@gmail.com>
2024-12-05 16:58:02 +0300

    guess

--- game.py
+++ game.py
@@ -1,37 +1,33 @@
 from random import randint
-class Guess:
-    def ask_for_guess(self):
-        try:
-            self.user_guess = int(input("Guess: "))
-        except ValueError:
-            self.ask_for_guess()
-        else:
-            self.check_user_guess()
-    
-    def ask_for_level(self):
-        try:
-            level: int = int(input("Level: "))
-        except ValueError:
-            self.ask_for_level()
-        else:
-            self.guess: int = randint(1, level)
         
-    def check_user_guess(self):
-        if self.user_guess == self.guess:
-            print("Just right!")
-        elif self.user_guess > self.guess:
-            print("Too large!")
-            self.ask_for_guess()
-        else:
-            print("Too small!")
-            self.ask_for_guess()
     
-    def main(self)-> None:
-            self.ask_for_level()
-            self.ask_for_guess()
-
+def main()-> None:
+            while True:
+                try:
+                    level: int = int(input("Level: "))
+                except ValueError:
+                    continue
+                else:
+                    if level < 1:
+                        continue
+                    else:
+                        guess: int = randint(1, level)
+                        while True:
+                            try:
+                                user_guess: int = int(input("Guess: "))
+                            except ValueError:
+                                continue
+                            else:
+                                if guess < user_guess:
+                                    print("Too large!")
+                                    continue
+                                elif guess > user_guess:
+                                    print("Too small!")
+                                    continue
+                                else:
+                                    print("Just right!")
+                                    break
+                break
     
 if __name__ == "__main__":
-    game = Guess()
-    
-    game.main()
+    main()
